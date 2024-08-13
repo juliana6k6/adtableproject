9542f51e2edec488be4ab48ee55e06ac5958f5fa
Juliana <juliana6k@mail.ru>
2024-08-13 22:49:44 +0300

    Модели users, ad, comment. Настройки.

---
+++ ads/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.utils import timezone
+from users.models import User
+
+
+class Ad(models.Model):
+    """Модель объявления должна содержать следующие поля:
+   - title — название товара;
+   - price — цена товара (целое число);
+   - description — описание товара;
+   - author — пользователь, который создал объявление;
+   - created_at — время и дата создания объявления.
+   Объявления должны быть отсортированы по дате создания (чем новее, тем выше).
+    """
+
+    title = models.CharField(max_length=150, default="нет названия товара",
+                             verbose_name="название товара", help_text="Укажите название товара"
+                             )
+    price = models.PositiveIntegerField(
+        default=0, verbose_name="цена товара", help_text="Укажите цену товара"
+    )
+    description = models.TextField(max_length=400, default='нет описания товара',
+                                   verbose_name="описание товара", help_text="Введите описание товара")
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='ad_author',
+        blank=True,
+        null=True,
+        verbose_name="Автор",
+        help_text="Автор объявления",
+    )
+    image = models.ImageField(upload_to="ads/", verbose_name="фото товара", help_text="Загрузите фото товара",
+                              blank=True, null=True)
+    created_at = models.DateField(
+        models.DateTimeField(default=timezone.now, verbose_name="дата и время создания")
+    )
+
+    def __str__(self):
+        return f"{self.title} - {self.price}"
+
+    class Meta:
+        verbose_name = "объявление"
+        verbose_name_plural = "объявления"
+        ordering = ('-created_at',)
+
+
+class Comment(models.Model):
+    """Модель отзыва должна содержать следующие поля:
+   - text — текст отзыва;
+   - author — пользователь, который оставил отзыв;
+   - ad — объявление, под которым оставлен отзыв;
+   - created_at — время и дата создания отзыва.
+   """
+
+    text = models.TextField(max_length=400, verbose_name="текст отзыва", help_text="Введите текст отзыва")
+    author = models.ForeignKey(
+        User, related_name='comment_author',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        verbose_name="Автор отзыва",
+    )
+    ad = models.ForeignKey(Ad, related_name='comment_ad', on_delete=models.CASCADE,
+                           verbose_name='объявление, под которым оставлен отзыв', null=True, blank=True)
+    created_at = models.DateField(
+        models.DateTimeField(default=timezone.now, verbose_name="дата и время создания")
+    )
+
+    def __str__(self):
+        return f"Отзыв от {self.created_at} написан {self.author}"
+
+    class Meta:
+        verbose_name = "отзыв"
+        verbose_name_plural = "отзывы"
+        ordering = ('-created_at',)
---
+++ users/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class UserRoles(models.TextChoices):
+    """Роли пользователя:
+    user - может получать список объявлений, получать одно объявление, создавать объявление,
+    редактировать и удалять свое объявление, получать список комментариев, создавать комментарии,
+    редактировать/удалять свои комментарии.
+    admin - может дополнительно к правам пользователя редактировать или удалять объявления и
+    комментарии любых других пользователей.
+    """
+
+    USER = "user"
+    ADMIN = "admin"
+
+
+class User(AbstractUser):
+    """Модель пользователя"""
+
+    username = None
+
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name="имя пользователя",
+        blank=True,
+        null=True,
+        help_text="Укажите имя",
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name="фамилия пользователя",
+        blank=True,
+        null=True,
+        help_text="Укажите фамилию",
+    )
+    phone_number = models.CharField(
+        max_length=35,
+        verbose_name="номер телефона",
+        blank=True,
+        null=True,
+        help_text="Укажите номер телефона",
+    )
+    avatar = models.ImageField(
+        upload_to="users/",
+        blank=True,
+        null=True,
+        verbose_name="Аватар",
+        help_text="Загрузите аватар",
+    )
+    email = models.EmailField(
+        unique=True, verbose_name="Email", help_text="Укажите электронную почту"
+    )
+    role = models.CharField(
+        max_length=10,
+        choices=UserRoles.choices,
+        default="user",
+        verbose_name="статус пользователя",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
