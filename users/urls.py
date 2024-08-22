c5eecc91a6b58b45a027afcfcea1b24d5afa35f0
Juliana <juliana6k@mail.ru>
2024-08-22 19:56:50 +0300

    Урлы джосера.

--- users/urls.py
+++ users/urls.py
@@ -5,15 +5,15 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
 from users.apps import UsersConfig
 
 """
-GET "users/djoser/users/" — список профилей пользователей
-POST "users/djoser/users/" — регистрация пользователя
-GET, PATCH, DELETE "users/djoser/users/{id}/" — в соотвествии с REST и необходимыми permissions (для администратора)
-GET PATCH "users/djoser/users/me/" — получение и изменение своего профиля
-POST "users/djoser/users/set_password/" — ручка для изменения пароля
-POST "users/djoser/users/reset_password/" — ручка для направления ссылки сброса пароля на email
-POST users/djoser/"users/reset_password_confirm/" — ручка для сброса своего пароля
-POST "users/djoser/token/" — альтернативное получение токена пользователя
-POST "users/djoser/token/refresh/" — альтернативное получение рефреш токена пользователя
+GET "api/users/djoser/users/" — список профилей пользователей
+POST "api/users/djoser/users/" — регистрация пользователя
+GET, PATCH, DELETE "api/users/djoser/users/{id}/" — в соотвествии с REST и необходимыми permissions (для администратора)
+GET PATCH "api/users/djoser/users/me/" — получение и изменение своего профиля
+POST "api/users/djoser/users/set_password/" — ручка для изменения пароля
+POST "api/users/djoser/users/reset_password/" — ручка для направления ссылки сброса пароля на email
+POST api/users/djoser/users/reset_password_confirm/" — ручка для сброса своего пароля
+POST "api/users/djoser/token/" — альтернативное получение токена пользователя
+POST "api/users/djoser/token/refresh/" — альтернативное получение рефреш токена пользователя
 """
 
 app_name = UsersConfig.name
